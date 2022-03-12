5a39eebde7bbd6627384ac5d9374925003ca64c1
hackerYHT <2418170957@qq.com>
2022-03-12 11:30:05 +0800

    v0.1

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,6 +44,13 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "akinsho/bufferline.nvim"
+  use "windwp/windline.nvim"
+
+ -- Colorschemes
+  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "lunarvim/darkplus.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
